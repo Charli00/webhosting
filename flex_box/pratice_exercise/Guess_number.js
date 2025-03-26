8b5d48ec22e806a8a8141a6df8ce14a30931e6d2
peniel charles p <98569512+Charli00@users.noreply.github.com>
2025-03-26 13:42:22 +0000

    hello

--- flex_box/pratice_exercise/Guess_number.js
+++ flex_box/pratice_exercise/Guess_number.js
@@ -5,14 +5,14 @@ function Get_user_Random(){
     
     let Random_number = Math.floor(Math.random() * 10 +1); //0 to 9
     console.log("Generated Random Number : "+Random_number)
-    if (isNaN(first_number) || first_number < 0 || first_number >9){
+    if (isNaN(first_number) || first_number < 0 || first_number >12){
         alert("Please enter the valid number : ");
         return;
     }
     if (first_number === Random_number) {
         user_enter_correct++; // Increase the score
         document.getElementById("Final_score").textContent =  "🎉 Congratulations! Your score is: " + user_enter_correct;
-        // alert("Correct! Your score is now: " + user_enter_correct);
+        // alert("Correct! Your score ids now: " + user_enter_correct);
     }
     else{
         console.log("Please enter another number one more")
