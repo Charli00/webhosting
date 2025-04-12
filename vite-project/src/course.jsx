dfe6aa7bf88da690b7f708966afe91047cc99698
peniel charles p <98569512+Charli00@users.noreply.github.com>
2025-04-12 10:55:17 +0000

    React new component

---
+++ vite-project/src/course.jsx
@@ -0,0 +1,12 @@
+function Course(){
+    return(
+        <div>
+
+            <h1>Python Programming</h1>
+            <p>Easy to learn the Programming</p>
+            <h2>C Prograaming</h2>
+        </div>
+    );
+
+}
+export default Course
---
+++ vite-project/src/navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar(){
+    return(
+        <nav>
+            <h1>Hello React Programming</h1>
+        </nav>
+    );
+}
+// Above statement is component
+export default Navbar
